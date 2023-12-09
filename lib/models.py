512d4586d92d488f85c3416d0f7ce64507778ca5
KELVIN MURITHI <kelvinmurithi@live.com>
2023-12-09 21:50:02 +0300

    defined Base

---
+++ lib/models.py
@@ -0,0 +1,58 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    star_rating = Column(Integer)
+
+    customer = relationship("Customer", back_populates="reviews")
+    restaurant = relationship("Restaurant", back_populates="reviews")
+
+    def full_review(self):
+        return f"Review for {self.restaurant.name} by {self.customer.full_name()}: {self.star_rating} stars."
+
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+    reviews = relationship("Review", back_populates="restaurant")
+    customers = relationship("Customer", secondary="reviews", back_populates="restaurants")
+
+    @classmethod
+    def fanciest(cls):
+        return session.query(cls).order_by(cls.price.desc()).first()
+
+    def all_reviews(self):
+        return [review.full_review() for review in self.reviews]
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    reviews = relationship("Review", back_populates="customer")
+    restaurants = relationship("Restaurant", secondary="reviews", back_populates="customers")
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def favorite_restaurant(self):
+        return session.query(Restaurant).join(Review).filter(Review.customer_id == self.id).order_by(Review.star_rating.desc()).first()
+
+    def add_review(self, restaurant, rating):
+        new_review = Review(customer_id=self.id, restaurant_id=restaurant.id, star_rating=rating)
+        session.add(new_review)
+        session.commit()
+
+    def delete_reviews(self, restaurant):
+        session.query(Review).filter_by(customer_id=self.id, restaurant_id=restaurant.id).delete()
+        session.commit()
